c900cb053359475c30d6d59b52c738a9f0351a81
Qi Shang <qishang1992@163.com>
2023-11-29 16:45:45 +0800

    1

--- 11_OLED_i2c/src/main.c
+++ 11_OLED_i2c/src/main.c
@@ -31,6 +31,7 @@
 #include "oled.h"
 #include "bmp.h"
 #include "GPIO.h"
+#include "Delay.h"
 
  int main(void)
  {	u8 t;
@@ -50,15 +51,39 @@
     OLED_ShowCHinese(108,0,6);//技
 	while(1) 
 	{		
-		OLED_Clear();
-		OLED_ShowCHinese(0,0,0);//中
+		// OLED_Clear();
+/* 		OLED_ShowCHinese(0,0,0);//中
 		OLED_ShowCHinese(18,0,1);//景
 		OLED_ShowCHinese(36,0,2);//园
 		OLED_ShowCHinese(54,0,3);//电
 		OLED_ShowCHinese(72,0,4);//子
 		OLED_ShowCHinese(90,0,5);//科
-		OLED_ShowCHinese(108,0,6);//技
-		OLED_ShowString(6,3,"0.96' OLED TEST",16);
+		OLED_ShowCHinese(108,0,6);//技 */
+        delay_ms(250);
+        OLED_ShowCHinese(0,2,0);//中
+		OLED_ShowCHinese(18,2,1);//景
+		OLED_ShowCHinese(36,2,2);//园
+		OLED_ShowCHinese(54,2,3);//电
+		OLED_ShowCHinese(72,2,4);//子
+		OLED_ShowCHinese(90,2,5);//科
+		OLED_ShowCHinese(108,2,6);//技
+        delay_ms(250);
+        OLED_ShowCHinese(0,4,0);//中
+		OLED_ShowCHinese(18,4,1);//景
+		OLED_ShowCHinese(36,4,2);//园
+		OLED_ShowCHinese(54,4,3);//电
+		OLED_ShowCHinese(72,4,4);//子
+		OLED_ShowCHinese(90,4,5);//科
+		OLED_ShowCHinese(108,4,6);//技
+        delay_ms(250);
+/*         OLED_ShowCHinese(0,6,0);//中
+		OLED_ShowCHinese(18,6,1);//景
+		OLED_ShowCHinese(36,6,2);//园
+		OLED_ShowCHinese(54,6,3);//电
+		OLED_ShowCHinese(72,6,4);//子
+		OLED_ShowCHinese(90,6,5);//科
+		OLED_ShowCHinese(108,6,6);//技 */
+		// OLED_ShowString(6,3,"0.96' OLED TEST",16);
 		//OLED_ShowString(8,2,"ZHONGJINGYUAN");  
 	 //	OLED_ShowString(20,4,"2014/05/01");  
 		OLED_ShowString(0,6,"ASCII:",16);  
@@ -67,7 +92,8 @@
 		t++;
 		if(t>'~')t=' ';
 		OLED_ShowNum(103,6,t,3,16);//显示ASCII字符的码值 	
-        delay_ms(8000);
+        delay_ms(250);
+/*         delay_ms(8000);
 		delay_ms(8000);
 
         delay_ms(8000);
@@ -82,7 +108,7 @@
 		delay_ms(8000);
         delay_ms(8000);
 		delay_ms(8000);
-		delay_ms(8000);
+		delay_ms(8000); */
 	}	  
 	
 }
